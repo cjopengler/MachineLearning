28d8bd8e1dda8a5544c0ad94d6bf7bfa1ce79068
panxu <panxu@baidu.com>
2016-03-07 18:10:20 +0800

    完成异常检测

---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,46 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+% 计算每个特征分量的均值，是将每一列相加求平均, sum(X) 会将列相加
+mu = sum(X) / m;
+
+% 计算方差
+% X每一个元素减去相应的均值
+subMu = bsxfun(@minus, X, mu);
+% 减去均值的做平方
+sqSubMu = subMu .* subMu;
+
+% 方差就是 平方相加/m
+sigma2 = sum(sqSubMu) / m;
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
---
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -0,0 +1,62 @@
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
+%outliers
+%   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval).
+%
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the F1 score of choosing epsilon as the
+    %               threshold and place the value in F1. The code at the
+    %               end of the loop will compare the F1 score for this
+    %               choice of epsilon and set it to be the best epsilon if
+    %               it is better than the current choice of epsilon.
+    %               
+    % Note: You can use predictions = (pval < epsilon) to get a binary vector
+    %       of 0's and 1's of the outlier predictions
+
+
+% 概率p 小于ε,说明是异常的 1表示；概率p 大于等于ε,说明是正常的 0表示；这种表述要和yval是一样的
+% 先计算基于概率的预测向量
+predictions = pval < epsilon;
+
+% 接下来计算查准率(precision)和召回率(recall)
+
+% 1 计算predictions与实际的y 相同以及不相同的地方，也就是真阳，假阳，真阴，假阴
+P = (predictions == yval);
+
+% 2 计算真阳性的数量, predictions 等于1 并且yval也等于1. predictions & yval 会只保留1 1 这种 其他的 1 0, 0 1, 0 0 都会变成0.
+turePositive = sum(predictions & yval);
+
+% 3 计算查准率
+positiveInPredictions = sum(predictions);
+prec = turePositive / positiveInPredictions;
+
+% 4 计算召回率
+positiveInY = sum(yval);
+rec = turePositive / positiveInY;
+
+% 5 计算F1
+F1 = 2 * prec * rec / (prec + rec);
+
+
+
+
+
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
